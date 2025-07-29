d8c61a1e93a8e139ff9977b56d18a625d6a9f61a
lolodev <horaciodev1706@gmail.com>
2025-07-29 17:04:36 -0300

    Update try...catch

--- index.js
+++ index.js
@@ -13,45 +13,37 @@ const timeEl = document.querySelector('#time');
 const weatherEl = document.querySelector('#weather');
 
 /* GET BACKGROUND IMAGE */
-async function getBackgroundImage() {
-    try {
-        const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=retail');
-        const data = await res.json();
-        bodyEl.style.backgroundImage = `url(${data.urls.regular})`;
-        authorEl.textContent = `By ${data.user.name}`;
-    } catch (error) {
-        console.log('Failed to fetch image');
-        bodyEl.style.backgroundImage = `url(./images/ivan-f-UH2AoKrvbcE-unsplash.jpg)`;
-        authorEl.textContent = `By Ivan F`;
-    };
-}
-
-getBackgroundImage();
+try {
+    const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=retail');
+    const data = await res.json();
+    bodyEl.style.backgroundImage = `url(${data.urls.regular})`;
+    authorEl.textContent = `By ${data.user.name}`;
+} catch (error) {
+    console.log('Failed to fetch image');
+    bodyEl.style.backgroundImage = `url(./images/ivan-f-UH2AoKrvbcE-unsplash.jpg)`;
+    authorEl.textContent = `By Ivan F`;
+};
 
 /* GET CURRENT DATA ON A CRYPTOCURRENCY */
-async function getCryptoData() {
-    try {
-        const res = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
-        if (!res.ok) {
-            const statusText = res.statusText || "No description available";
-            throw new Error(`Error ${res.status} - ${statusText}`);
-        }
-        const data = await res.json();
-        cryptoTopEl.innerHTML = `
+
+try {
+    const res = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
+    if (!res.ok) {
+        const statusText = res.statusText || "No description available";
+        throw new Error(`Error ${res.status} - ${statusText}`);
+    }
+    const data = await res.json();
+    cryptoTopEl.innerHTML = `
             <img src="${data.image.small}"><p>${data.name}</p>`;
-        cryptoEndEl.innerHTML = `
+    cryptoEndEl.innerHTML = `
         <p class="crypto">🎯 Price: $${data.market_data.current_price.ars}</p>
         <p class="crypto">📈 High: $${data.market_data.high_24h.ars}</p>
         <p class="crypto">📉 Low: $${data.market_data.low_24h.ars}</p>
         `;
-    } catch (error) {
-        cryptoTopEl.innerHTML = `
+} catch (error) {
+    cryptoTopEl.innerHTML = `
             <p p class="crypto" >❌ ${error.message}</p> `;
-    }
-
 }
-
-getCryptoData();
 
 /* GET TIME */
 setInterval(updateTime, 1000);
